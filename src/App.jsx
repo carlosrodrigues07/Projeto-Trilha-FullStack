e8df380725aa28817f36f7a3258643f48836096b
Carlos Henrique <carlospaixao05@gmail.com>
2025-07-10 19:23:05 -0300

    ✨ feat: atualizando componente App.jsx com nova lógica ou layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
-import Home from "./components/Home"
+// src/App.jsx
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "/src/pages/Home";
+import ProductDetails from "/src/pages/ProductDetails";
+import Cart from "/src/pages/Card.jsx";
+import Header from "/src/components/Header";
+import Footer from "/src/components/Footer";
+import { CartProvider } from "/src/context/CardContext";
+
 
 function App() {
- 
-
   return (
-    <>
-      <Home />
-    </>
-  )
+    <CartProvider>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/produto/:id" element={<ProductDetails />} />
+          <Route path="/carrinho" element={<Cart />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </CartProvider>
+  );
 }
 
-export default App
+export default App;
