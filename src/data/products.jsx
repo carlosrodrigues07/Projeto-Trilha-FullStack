919e16f1169616bb3c5f90a0c2f196274fddf58b
DimasGabriel1 <dimasg489@gmail.com>
2025-07-15 00:13:29 -0300

    100 imagens de produtos

--- src/data/products.jsx
+++ src/data/products.jsx
@@ -1,10 +1,9 @@
-// src/data/products.js
 const products = Array.from({ length: 100 }, (_, i) => ({
   id: i + 1,
   name: `Produto ${i + 1}`,
   description: `Descrição completa do Produto ${i + 1}.`,
   price: (5 + i * 0.75).toFixed(2),
-  image: `/src/assets/imagens${i}.png`, 
+  image: `/images/imagens${i}.jpg`, 
 }));
 
 export default products;
